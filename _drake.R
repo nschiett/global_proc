94f3efe6816f105014496ee95ed392e841cb6e24
nschiett <nina.schiettekatte@gmail.com>
2020-04-28 17:53:22 +0200

    adding new diet categories parravicini et al. 2020

---
+++ _drake.R
@@ -0,0 +1,6 @@
+source("R/packages.R")  # loads packages
+source("R/functions_wrangling.R") # defines the create_plot() function
+source("R/functions_analysis.R") # defines the create_plot() function
+source("R/plan.R")      # creates the drake plan
+
+config <- drake_config(plan, lock_envir = FALSE)
